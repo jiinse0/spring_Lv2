cca9b4ad9d611ff526638f779e860e5c63e37ca1
jiinse0 <jee_ji@naver.com>
2023-07-12 18:13:54 +0900

    user

--- src/main/java/com/sparta/post/controller/UserController.java
+++ src/main/java/com/sparta/post/controller/UserController.java
@@ -1,31 +1,54 @@
 package com.sparta.post.controller;
 
-import com.sparta.post.dto.LoginRequestDto;
-import com.sparta.post.dto.SignupRequestDto;
-import com.sparta.post.dto.UserResponseDto;
+import com.sparta.post.dto.AuthRequestDto;
+import com.sparta.post.dto.StatusResponseDto;
+import com.sparta.post.jwt.JwtUtil;
 import com.sparta.post.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
+    private final JwtUtil jwtUtil;
+
     // 회원가입
-    @PostMapping("/signup")
-    public UserResponseDto signup(@RequestBody SignupRequestDto signupRequestDto) {
-        return userService.signup(signupRequestDto);
+    @PostMapping("/auth/signup")
+    public StatusResponseDto signup(@Valid @RequestBody AuthRequestDto requestDto) {
+
+        userService.signup(requestDto);
+
+        return new StatusResponseDto("회원가입이 완료되었습니다.", HttpStatus.CREATED.value());
     }
 
     // 로그인
-    @PostMapping("/login")
-    public UserResponseDto login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse httpServletResponse) {
-        return userService.login(loginRequestDto, httpServletResponse);
+    @PostMapping("/auth/login")
+    // HttpServletResponse : 서블릿 컨테이너에서 클라이언트로부터 받은 HTTP 요청에 대한 응답을 생성하고 전송하기 위한 객체
+    public StatusResponseDto login(@RequestBody AuthRequestDto requestDto, HttpServletResponse response) {
+
+        userService.login(requestDto);
+
+        /*
+        JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        response.addHeader() : response 객체에 새로운 헤더를 추가
+                               JwtUtil.AUTHORIZATION_HEADER에 정의된 헤더 이름과 jwtUtil.createToken()에서 생성한 JWT 토큰 값을 사용하여 헤더를 설정
+                               HTTP 응답에 JWT 토큰이 포함된 헤더가 추가
+
+        JwtUtil.AUTHORIZATION_HEADER : JwtUtil 클래스에 있는 AUTHORIZATION_HEADER라는 상수값을 참조.
+        AUTHORIZATION_HEADER : JWT 토큰을 HTTP 요청의 헤더에 추가할 때 사용되는 헤더 이름
+        jwtUtil.createToken(loginRequestDto.getUsername()) : jwtUtil 객체를 사용하여 AuthRequestDto에서 가져온 사용자 이름을 기반으로 JWT 토큰 생성
+        */
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(requestDto.getUsername()));
+
+        return new StatusResponseDto("로그인이 완료되었습니다.", HttpStatus.OK.value());
     }
 }
--- src/main/java/com/sparta/post/service/UserService.java
+++ src/main/java/com/sparta/post/service/UserService.java
@@ -1,73 +1,44 @@
 package com.sparta.post.service;
 
-import com.sparta.post.dto.LoginRequestDto;
-import com.sparta.post.dto.SignupRequestDto;
-import com.sparta.post.dto.UserResponseDto;
+import com.sparta.post.dto.AuthRequestDto;
 import com.sparta.post.entity.User;
-import com.sparta.post.jwt.JwtUtil;
 import com.sparta.post.repository.UserRepository;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
-    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
 
-    // 회원가입
-    @Transactional
-    public UserResponseDto signup(SignupRequestDto requestDto) {
+    public void signup(AuthRequestDto requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
+        String password = passwordEncoder.encode(requestDto.getPassword());
 
-        // id 중복 확인
-        Optional<User> checkUsername = userRepository.findByUsername(username);
-        if (checkUsername.isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        /*isPresent : Optional 객체가 값(Non-null)을 포함하고 있는지 여부를 확인하는 메서드
+        값이 존재하는 경우 true를 반환하고, 값이 없는 경우(null 또는 Optional.empty()) false를 반환*/
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
         }
 
-        userRepository.save(new User(username, password));
-
-        // 회원가입 완료 로그 출력
-        log.info("회원가입이 완료되었습니다.");
-
-        UserResponseDto userResponseDto = new UserResponseDto("회원가입이 완료 되었습니다.", HttpStatus.OK.value());
-        return userResponseDto;
+        User user = new User(username, password);
+        userRepository.save(user);
     }
-    // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
 
-    // 로그인
-    @Transactional(readOnly = true)
-    public UserResponseDto login(LoginRequestDto requestDto, HttpServletResponse res) {
+    public void login(AuthRequestDto requestDto) {
         String username = requestDto.getUsername();
         String password = requestDto.getPassword();
 
-        // username 일치 여부 확인
         User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않은 아이디입니다.")
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
         );
 
-        // password 확인
-        if (!user.getPassword().equals(password)) {
+        //passwordEncoder.matches()를 사용하면 인코딩된 비밀번호와 주어진 비밀번호가 일치하는지 확인할 수 있다.
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-
-        // 토큰 생성 및 쿠키에 저장
-        res.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
-
-        // 로그인 성공 로그 출력
-        log.info("로그인에 성공하셨습니다.");
-
-        // UserResponseDto 생성 및 반환
-        UserResponseDto userResponseDto = new UserResponseDto("로그인 성공하셨습니다.", HttpStatus.OK.value());
-        return userResponseDto;
     }
 }
